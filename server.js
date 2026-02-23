b72192ef82a9e2f5876792b2e4ee4bf6cf62e959
Abhay Vishwakarma <hb9868313@gmail.com>
2026-02-23 08:20:19 +0530

    Done

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
   origin: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
